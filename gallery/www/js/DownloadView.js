f237280ad2c2fa798887cda1c217437675b1e8f3
cicelybeckford <cicelybeckford@users.noreply.github.com>
2017-12-12 22:52:42 -0500

    Added assets folder  and added write file to download view

--- gallery/www/js/DownloadView.js
+++ gallery/www/js/DownloadView.js
@@ -11,11 +11,12 @@ var DownloadView = function(app) {
 	function downloadFile() {
 		var fileTransfer = new FileTransfer();
 		var uri = encodeURI($('.url').val().trim());
+		var url = $('.url').val().trim();
 		var tag = $('.tag').val().trim().toLowerCase();
 		var fileURL = cordova.file.cacheDirectory + tag; // where to save
 		fileTransfer.download(uri, fileURL, function(entry) {
 			console.log("download complete: " + entry.toURL());
-			localStorage.setItem(tag, fileURL);
+			writeFile(url, tag);
 		},
 		function(error) {
 			alert("download error source " + error.source);
@@ -27,6 +28,28 @@ var DownloadView = function(app) {
 			headers: {"Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="}
 		}
 		);
+	}
+	
+	function writeFile(url, tag) {
+		var type = window.TEMPORARY;
+		var size = 5*1024*1024;
+		window.requestFileSystem(type, size, successCallback, errorCallback);
+		function successCallback(fs) {
+			var fileName = (tag + url.substr(url.lastIndexOf('/')+1) + '.txt');
+			fs.root.getFile(fileName, {create: true}, function(fileEntry) {
+				fileEntry.createWriter(function(fileWriter) {
+					localStorage.setItem(tag, fileName);
+					console.log(fileName);
+					fileWriter.onwriteend = function(e) { alert('Write completed.');};
+					fileWriter.onerror = function(e) {
+						alert('Write failed: ' + e.toString());
+					};
+					var blob = new Blob([url], {type: 'text/plain'});
+					fileWriter.write(blob);
+				}, errorCallback);
+			}, errorCallback);
+		}
+		function errorCallback(error) { alert("ERROR: " + error.code) }
 	}
 	
 	this.initialize();
